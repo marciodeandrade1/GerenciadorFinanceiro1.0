425cbe23032ee0dec21f55bac481230416a14528
Marcio de Andrade <marcio@requestdev.net>
2025-02-08 17:55:36 -0400

    Add project files.

---
+++ GerenciadorFinanceiro/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace GerenciadorFinanceiro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.lblDataTransacao = new System.Windows.Forms.Label();
+            this.dgvTransacoes = new System.Windows.Forms.DataGridView();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.dtpData = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransacoes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(479, 299);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(97, 18);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(275, 20);
+            this.txtDescricao.TabIndex = 1;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(97, 57);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 2;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Location = new System.Drawing.Point(33, 21);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(58, 13);
+            this.lblDescricao.TabIndex = 4;
+            this.lblDescricao.Text = "Descrição:";
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(34, 57);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(34, 13);
+            this.lblValor.TabIndex = 5;
+            this.lblValor.Text = "Valor:";
+            // 
+            // lblDataTransacao
+            // 
+            this.lblDataTransacao.AutoSize = true;
+            this.lblDataTransacao.Location = new System.Drawing.Point(240, 63);
+            this.lblDataTransacao.Name = "lblDataTransacao";
+            this.lblDataTransacao.Size = new System.Drawing.Size(33, 13);
+            this.lblDataTransacao.TabIndex = 6;
+            this.lblDataTransacao.Text = "Data:";
+            this.lblDataTransacao.Click += new System.EventHandler(this.lblDataTransacao_Click);
+            // 
+            // dgvTransacoes
+            // 
+            this.dgvTransacoes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTransacoes.Location = new System.Drawing.Point(36, 121);
+            this.dgvTransacoes.Name = "dgvTransacoes";
+            this.dgvTransacoes.Size = new System.Drawing.Size(518, 172);
+            this.dgvTransacoes.TabIndex = 7;
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "Receita",
+            "Despesa"});
+            this.cmbTipo.Location = new System.Drawing.Point(393, 18);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipo.TabIndex = 8;
+            // 
+            // dtpData
+            // 
+            this.dtpData.Location = new System.Drawing.Point(279, 57);
+            this.dtpData.Name = "dtpData";
+            this.dtpData.Size = new System.Drawing.Size(235, 20);
+            this.dtpData.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 334);
+            this.Controls.Add(this.dtpData);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.dgvTransacoes);
+            this.Controls.Add(this.lblDataTransacao);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.btnSalvar);
+            this.Name = "Form1";
+            this.Text = "Gerenciador Financeiro 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransacoes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          //  throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Label lblDataTransacao;
+        private System.Windows.Forms.DataGridView dgvTransacoes;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.DateTimePicker dtpData;
+    }
+}
+
---
+++ GerenciadorFinanceiro/Form1.cs
@@ -0,0 +1,82 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciadorFinanceiro
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "server=localhost;database=GerenciadorFinanceiro;userid=root;password=@Divinh@2025;";
+        public Form1()
+        {
+            InitializeComponent();
+            CarregarTransacoes();
+        }
+
+        private void CarregarTransacoes()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM Transacoes";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgvTransacoes.DataSource = dt;
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            // Validação de entrada
+            if (string.IsNullOrWhiteSpace(txtDescricao.Text))
+            {
+                MessageBox.Show("A descrição não pode estar vazia.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!decimal.TryParse(txtValor.Text, out decimal valor) || valor <= 0)
+            {
+                MessageBox.Show("O valor deve ser um número positivo.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (cmbTipo.SelectedItem == null || (cmbTipo.SelectedItem.ToString() != "Receita" && cmbTipo.SelectedItem.ToString() != "Despesa"))
+            {
+                MessageBox.Show("Selecione um tipo válido (Receita ou Despesa).", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string descricao = txtDescricao.Text;
+            string tipo = cmbTipo.SelectedItem.ToString();
+            DateTime data = dtpData.Value;
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO Transacoes (Descricao, Valor, Tipo, Data) VALUES (@Descricao, @Valor, @Tipo, @Data)";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Descricao", descricao);
+                cmd.Parameters.AddWithValue("@Valor", valor);
+                cmd.Parameters.AddWithValue("@Tipo", tipo);
+                cmd.Parameters.AddWithValue("@Data", data);
+                cmd.ExecuteNonQuery();
+            }
+
+            CarregarTransacoes();
+        }
+
+        private void lblDataTransacao_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+ 
+}
